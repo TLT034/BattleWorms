151d768eccf8d1210a8fc09e6ed9137f4e5086a0
Tristan Thomas <tristanlt034@gmail.com>
2020-01-08 17:23:18 -0700

    reorganized code with worm class, etc. Implemented custom scoring

--- wormy.py
+++ wormy.py
@@ -10,6 +10,76 @@ import sys
 
 from pygame.locals import *
 
+
+class Worm:
+    def __init__(self, color, outline_color, start_direction, start_coord, controls):
+        self.color = color
+        self.outline_color = outline_color
+        self.direction = start_direction
+        self.coords = [{'x': start_coord[0], 'y': start_coord[1]},
+                       {'x': start_coord[0] - 1, 'y': start_coord[1]},
+                       {'x': start_coord[0] - 2, 'y': start_coord[1]}]
+        self.controls = controls
+        self.alive = True
+        self.time_alive = 0
+
+    def control_worm(self, key_input):
+        # checks key input against the controls and returns a direction
+        if (key_input == self.controls['left']) and self.direction != RIGHT:
+            self.direction = LEFT
+        elif (key_input == self.controls['right']) and self.direction != LEFT:
+            self.direction = RIGHT
+        elif (key_input == self.controls['up']) and self.direction != DOWN:
+            self.direction = UP
+        elif (key_input == self.controls['down']) and self.direction != UP:
+            self.direction = DOWN
+        elif key_input == K_ESCAPE:
+            terminate()
+
+    def check_death(self):
+        # check if the worm has hit itself or the edge
+        if self.coords[HEAD]['x'] == -1 or self.coords[HEAD]['x'] == CELLWIDTH or self.coords[HEAD]['y'] == -1 or \
+                self.coords[HEAD]['y'] == CELLHEIGHT:
+            self.alive = False
+
+        for wormBody in self.coords[1:]:
+            if wormBody['x'] == self.coords[HEAD]['x'] and wormBody['y'] == self.coords[HEAD]['y']:
+                self.alive = False
+
+    def check_grow(self, apples):
+        # check if worm has eaten an apple
+        no_apples_eaten = True
+        new_apples = apples
+
+        for i in range(len(apples)):
+            if self.coords[HEAD]['x'] == apples[i]['x'] and self.coords[HEAD]['y'] == apples[i]['y']:
+                # don't remove worm's tail segment
+                new_apples[i] = getRandomLocation()  # set a new apple somewhere
+                no_apples_eaten = False
+
+        if no_apples_eaten:
+            del self.coords[-1]  # remove worm's tail segment
+
+        return new_apples
+
+    def change_worm_head(self):
+        # move the worm by adding a segment in the direction it is moving
+        if self.direction == UP:
+            new_head = {'x': self.coords[HEAD]['x'], 'y': self.coords[HEAD]['y'] - 1}
+        elif self.direction == DOWN:
+            new_head = {'x': self.coords[HEAD]['x'], 'y': self.coords[HEAD]['y'] + 1}
+        elif self.direction == LEFT:
+            new_head = {'x': self.coords[HEAD]['x'] - 1, 'y': self.coords[HEAD]['y']}
+        elif self.direction == RIGHT:
+            new_head = {'x': self.coords[HEAD]['x'] + 1, 'y': self.coords[HEAD]['y']}
+
+        self.coords.insert(0, new_head)
+
+    def __del__(self):
+        print('Deleting Worm')
+
+
+
 NUM_APPLES = 3
 FPS = 15
 WINDOWWIDTH = 1280
@@ -26,6 +96,7 @@ WHITE     = (255, 255, 255)
 BLACK     = (  0,   0,   0)
 RED       = (255,   0,   0)
 BLUE      = (  0,   0, 255)
+DARKBLUE  = (  0,   0, 155)
 GREEN     = (  0, 255,   0)
 DARKGREEN = (  0, 155,   0)
 DARKGRAY  = ( 40,  40,  40)
@@ -50,128 +121,85 @@ def main():
 
     showStartScreen()
     while True:
-        runGame()
+        # create worms before each game
+        w1 = Worm(
+            color=GREEN,
+            outline_color=DARKGREEN,
+            start_direction=UP,
+            start_coord=(6, CELLHEIGHT - 7),
+            controls={'left': K_a, 'right': K_d, 'up': K_w, 'down': K_s}
+        )
+
+        w2 = Worm(
+            color=BLUE,
+            outline_color=DARKBLUE,
+            start_direction=DOWN,
+            start_coord=(CELLWIDTH - 7, 6),
+            controls={'left': K_LEFT, 'right': K_RIGHT, 'up': K_UP, 'down': K_DOWN}
+        )
+
+        worms = [w1, w2]
+        runGame(worms)
         showGameOverScreen()
 
 
-def runGame():
-    game_over1 = False
-    game_over2 = False
-
-    # Set a random start point.
-    startx = 6
-    starty = CELLHEIGHT - 7
-    p1_wormCoords = [{'x': startx, 'y': starty}, {'x': startx - 1, 'y': starty}, {'x': startx - 2, 'y': starty}]
-    p1_direction = UP
-
-    startx2 = CELLWIDTH - 7
-    starty2 = 6
-    p2_wormCoords = [{'x': startx2, 'y': starty2}, {'x': startx2 - 1, 'y': starty2}, {'x': startx2 - 2, 'y': starty2}]
-    p2_direction = DOWN
+def runGame(worms):
+    game_over = False
+    time_ms = 0
 
     # Initialize list of random apples
     apples = []
     for i in range(NUM_APPLES):
         apples.append(getRandomLocation())
 
-    print("Apples:", apples)
-
-    while not game_over1 and not game_over2:  # main game loop
-        for event in pygame.event.get(): # event handling loop
+    while not game_over:  # main game loop
+        for event in pygame.event.get():  # event handling loop
             if event.type == QUIT:
                 terminate()
             elif event.type == KEYDOWN:
-                p1_direction = controlWorm(p1_direction, event.key, player=1)
-                p2_direction = controlWorm(p2_direction, event.key, player=2)
+                # if the worm is alive, get user input for controlling the worm
+                for w in worms:
+                    if w.alive:
+                        w.control_worm(event.key)
 
+        # if worms are alive, check if they hit a wall, themselves, or an apple,
+        # then update death, growth and movement accordingly
+        num_dead_worms = 0
+        for w in worms:
+            if w.alive:
+                w.check_death()
+                w.check_grow(apples)
+                w.change_worm_head()
+            else:
+                num_dead_worms += 1
 
-        game_over1 = checkGameOver(p1_wormCoords)
-        game_over2 = checkGameOver(p2_wormCoords)
-        p1_wormCoords, apples = checkAppleEat(p1_wormCoords, apples)
-        p2_wormCoords, apples = checkAppleEat(p2_wormCoords, apples)
+        if time_ms >= 1000:
+            for w in worms:
+                if w.alive:
+                    w.time_alive += 1
+            time_ms = 0
 
-
-        p1_newHead = changeWormHead(p1_direction, p1_wormCoords)
-        p2_newHead = changeWormHead(p2_direction, p2_wormCoords)
-
-        p1_wormCoords.insert(0, p1_newHead)  # have already removed the last segment
-        p2_wormCoords.insert(0, p2_newHead)  # have already removed the last segment
+        # end game if all worms are dead
+        if num_dead_worms == len(worms):
+            game_over = True
+            print('Game Over!')
+            for w in worms:
+                w.__del__()
 
         DISPLAYSURF.fill(BGCOLOR)
         drawGrid()
-        drawWorm(p1_wormCoords)
-        drawWorm(p2_wormCoords)
+        for w in worms:
+            drawWorm(w)
         for apple in apples:
             drawApple(apple)
-        drawScore(len(p1_wormCoords) - 3)
+        drawScore(worms)
         pygame.display.update()
-        FPSCLOCK.tick(FPS)
-
-
-
-def controlWorm(direction, keyInput, player):
-    if (keyInput == K_LEFT and player == 2 or keyInput == K_a and player == 1) and direction != RIGHT:
-        direction = LEFT
-    elif (keyInput == K_RIGHT and player == 2 or keyInput == K_d and player == 1) and direction != LEFT:
-        direction = RIGHT
-    elif (keyInput == K_UP and player == 2 or keyInput == K_w and player == 1) and direction != DOWN:
-        direction = UP
-    elif (keyInput == K_DOWN and player == 2 or keyInput == K_s and player == 1) and direction != UP:
-        direction = DOWN
-    elif keyInput == K_ESCAPE:
-        terminate()
-
-    return direction
-
-
-
-def changeWormHead(direction, wormCoords):
-    # move the worm by adding a segment in the direction it is moving
-    if direction == UP:
-        head = {'x': wormCoords[HEAD]['x'], 'y': wormCoords[HEAD]['y'] - 1}
-    elif direction == DOWN:
-        head = {'x': wormCoords[HEAD]['x'], 'y': wormCoords[HEAD]['y'] + 1}
-    elif direction == LEFT:
-        head = {'x': wormCoords[HEAD]['x'] - 1, 'y': wormCoords[HEAD]['y']}
-    elif direction == RIGHT:
-        head = {'x': wormCoords[HEAD]['x'] + 1, 'y': wormCoords[HEAD]['y']}
-
-    return head
-
-
-
-def checkGameOver(wormCoords):
-    # check if the worm has hit itself or the edge
-    if wormCoords[HEAD]['x'] == -1 or wormCoords[HEAD]['x'] == CELLWIDTH or wormCoords[HEAD]['y'] == -1 or \
-            wormCoords[HEAD]['y'] == CELLHEIGHT:
-        return True  # game over
-    for wormBody in wormCoords[1:]:
-        if wormBody['x'] == wormCoords[HEAD]['x'] and wormBody['y'] == wormCoords[HEAD]['y']:
-            return True  # game over
-
-    return False  # Not game over
-
-
-
-def checkAppleEat(wormCoords, apples):
-    # check if worm has eaten an apple
-    no_apples_eaten = True
-
-    for i in range(len(apples)):
-        if wormCoords[HEAD]['x'] == apples[i]['x'] and wormCoords[HEAD]['y'] == apples[i]['y']:
-            # don't remove worm's tail segment
-            apples[i] = getRandomLocation()  # set a new apple somewhere
-            no_apples_eaten = False
-
-    if no_apples_eaten:
-        del wormCoords[-1]  # remove worm's tail segment
-
-    return wormCoords, apples
-
+        ms_per_frame = FPSCLOCK.tick(FPS)
+        time_ms += ms_per_frame
 
 
 def drawPressKeyMsg():
-    pressKeySurf = BASICFONT.render('Press a key to play.', True, YELLOW)
+    pressKeySurf = BASICFONT.render('Press any key to play.', True, YELLOW)
     pressKeyRect = pressKeySurf.get_rect()
     pressKeyRect.topleft = (WINDOWWIDTH - 200, WINDOWHEIGHT - 30)
     DISPLAYSURF.blit(pressKeySurf, pressKeyRect)
@@ -242,28 +270,33 @@ def showGameOverScreen():
     drawPressKeyMsg()
     pygame.display.update()
     pygame.time.wait(500)
-    checkForKeyPress() # clear out any key presses in the event queue
+    checkForKeyPress()  # clear out any key presses in the event queue
 
     while True:
         if checkForKeyPress():
-            pygame.event.get() # clear event queue
+            pygame.event.get()  # clear event queue
             return
 
-def drawScore(score):
-    scoreSurf = BASICFONT.render('Score: %s' % (score), True, WHITE)
-    scoreRect = scoreSurf.get_rect()
-    scoreRect.topleft = (WINDOWWIDTH - 120, 10)
-    DISPLAYSURF.blit(scoreSurf, scoreRect)
+def drawScore(worms):
+    prev_y_location = -10  # used for location of each score display
+    for i in range(len(worms)):
+        # Scoring: 100pts per apple eaten, 5pts per second alive
+        score = ((len(worms[i].coords) - 3) * 100) + (worms[i].time_alive * 5)
+        scoreSurf = BASICFONT.render(f'Worm {i+1} Score: {score}', True, WHITE)
+        scoreRect = scoreSurf.get_rect()
+        scoreRect.topleft = (WINDOWWIDTH - 180, prev_y_location + 25)
+        prev_y_location += 25
+        DISPLAYSURF.blit(scoreSurf, scoreRect)
 
 
-def drawWorm(wormCoords):
-    for coord in wormCoords:
+def drawWorm(worm):
+    for coord in worm.coords:
         x = coord['x'] * CELLSIZE
         y = coord['y'] * CELLSIZE
         wormSegmentRect = pygame.Rect(x, y, CELLSIZE, CELLSIZE)
-        pygame.draw.rect(DISPLAYSURF, DARKGREEN, wormSegmentRect)
+        pygame.draw.rect(DISPLAYSURF, worm.outline_color, wormSegmentRect)
         wormInnerSegmentRect = pygame.Rect(x + 4, y + 4, CELLSIZE - 8, CELLSIZE - 8)
-        pygame.draw.rect(DISPLAYSURF, GREEN, wormInnerSegmentRect)
+        pygame.draw.rect(DISPLAYSURF, worm.color, wormInnerSegmentRect)
 
 
 def drawApple(coord):
